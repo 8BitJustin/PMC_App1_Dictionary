b1c7f5716aec5eac469cc49e3af7b949de0bbd95
Justin Olson <j.olson.digital@gmail.com>
2019-07-11 16:26:32 -0700

    Project complete. Bonus, added capitalize() for proper nouns.

--- search.py
+++ search.py
@@ -1,13 +1,32 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
-def dict_search():
-    search = input("Word to search for: ")
-    search = search.lower()
-    if search in data:
-        return data[search]
-    else:
-        return "Word doesn't exist within dictionary."
+word = input("Word to search for: ")
 
-print(dict_search())
+def dict_search(word):
+    if word.capitalize() in data:
+        return data[word.capitalize()]
+    else:
+        word = word.lower()
+        if word in data:
+            return data[word]
+        elif len(get_close_matches(word, data.keys())) > 0:
+            run = input("Did you mean %s? " % get_close_matches(word, data.keys())[0])
+            if run.lower() == 'y':
+                return data[get_close_matches(word, data.keys())[0]]
+            elif run.lower() == 'n':
+                return "Word doesn't exist within dictionary."
+            else:
+                return "Entry not understood."
+        else:
+            return "Word doesn't exist within dictionary."
+
+output = dict_search(word)
+
+if type(output) == list:
+    for i in output:
+        print(i)
+else:
+    print(output)
