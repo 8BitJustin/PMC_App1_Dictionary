bcef0cec315fcb887b0dc6c06a70ca4e06e95e4e
8bitjustin <j.olson.digital@gmail.com>
2019-07-12 08:40:08 -0700

    Added .upper() conditional for uppercase inquiries.

--- search.py
+++ search.py
@@ -6,22 +6,23 @@ data = json.load(open("data.json"))
 word = input("Word to search for: ")
 
 def dict_search(word):
-    if word.capitalize() in data:
-        return data[word.capitalize()]
-    else:
-        word = word.lower()
-        if word in data:
-            return data[word]
-        elif len(get_close_matches(word, data.keys())) > 0:
-            run = input("Did you mean %s? " % get_close_matches(word, data.keys())[0])
-            if run.lower() == 'y':
-                return data[get_close_matches(word, data.keys())[0]]
-            elif run.lower() == 'n':
-                return "Word doesn't exist within dictionary."
-            else:
-                return "Entry not understood."
+    word = word.lower()
+    if word in data:
+        return data[word]
+    elif word.upper() in data:
+        return data[word.upper()]
+    elif word.title() in data:  # if user entered "texas" this will check for "Texas" as well.
+        return data[word.title()]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        run = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(word, data.keys())[0])
+        if run.lower() == "y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif run.lower() == "n":
+            return "The word doesn't exist. Please double check it."
         else:
-            return "Word doesn't exist within dictionary."
+            return "We didn't understand your entry."
+    else:
+        return "The word doesn't exist. Please double check it."
 
 output = dict_search(word)
 
